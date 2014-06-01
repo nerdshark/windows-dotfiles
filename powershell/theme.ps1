e466fd461ca78fa404d0d86eb9736d1a4207460d
Dustin Mays <dustin.nathaniel.mays@gmail.com>
2014-06-01 02:22:46 -0400

    moving main profile config into repo - powershell profile should now only source this file

---
+++ powershell/theme.ps1
@@ -0,0 +1,31 @@
+﻿$pathsToAddToPath = @("C:\Program Files (x86)\Git\bin", "C:\chocolatey\bin")
+
+foreach ($path in $pathsToAddToPath)
+{
+	if ($env:Path -notlike "*" + $path + "*")
+	{
+		$env:Path += ";"
+		$env:Path += $path
+	}
+}
+
+Import-Module "$PSScriptRoot\posh-git"
+. "$PSScriptRoot\posh-git\profile.example.ps1"
+
+. "$PSScriptRoot\powershell-ise-solarized\Solarize-PSISE-AddOnMenu.ps1" -Apply -Dark
+
+$psISE.Options.FontName = "Source Code Pro"
+$psISE.Options.FontSize = 8
+
+if (-not $alreadyStarted)
+{
+    set-location $HOME
+    $alreadyStarted = $true
+}
+
+function reload-profile
+{
+    push-location
+    . $profile
+    pop-location
+}
