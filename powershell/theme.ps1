287082298122121e33a3eaaf6a316c2dd58ceec9
Dustin Mays <dork.fish.wat@gmail.com>
2014-06-01 17:01:17 -0400

    imported Get-AdminStatus module and sourced the prompt file

--- powershell/theme.ps1
+++ powershell/theme.ps1
@@ -1,4 +1,6 @@
-﻿$pathsToAddToPath = @("C:\Program Files (x86)\Git\bin", "C:\chocolatey\bin")
+﻿Import-Module "$PSScriptRoot\Get-AdminStatus"
+
+$pathsToAddToPath = @("C:\Program Files (x86)\Git\bin", "C:\chocolatey\bin")
 
 foreach ($path in $pathsToAddToPath)
 {
@@ -10,11 +12,12 @@ foreach ($path in $pathsToAddToPath)
 }
 
 Import-Module "$PSScriptRoot\posh-git"
-. "$PSScriptRoot\posh-git\profile.example.ps1"
+#. "$PSScriptRoot\posh-git\profile.example.ps1"
+. "$PSScriptRoot\prompt.ps1"
 
 . "$PSScriptRoot\powershell-ise-solarized\Solarize-PSISE-AddOnMenu.ps1" -Apply -Dark
 
-$psISE.Options.FontName = "Source Code Pro"
+$psISE.Options.FontName = "Source Code Pro for Powerline"
 $psISE.Options.FontSize = 8
 
 if (-not $alreadyStarted)
